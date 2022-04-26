9828ce055787f467fb5634a30d868f727fc754f9
Jalam Dangi <JalamD@bpl.netlink.com>
2022-04-26 18:19:03 +0530

    some changes

---
+++ db/config.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jalamdangi:jalamdangi123@cluster0.ljes2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const { response } = require('express');
 const express = require('express');
+require('./db/config');
+const User = require('./db/User');
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("App is working");
+// app.get("/", (req, res)=>{
+//     res.send("App is working");
+// })
+
+app.post("/register", async(req,res)=>{
+    res.send(req.body);
+    let user = new User(req.body);
+    let result = await user.save();
+    response.send(result);
 })
 
 app.listen(5000);
